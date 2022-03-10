7b272a1f4500bf96363423977f6a18ad870403e4
TaimourArshad1 <80366006+TaimourArshad1@users.noreply.github.com>
2022-03-09 19:45:22 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -27,8 +27,8 @@ extern void list_contents(char* path){
 			printf("%s\n", dir->d_name);
 		}
 		closedir(d);
-		}
-		}
+	}
+}
 	
 extern void cd_command(char* cur, char* path){
 	int c = chdir(path);
@@ -36,12 +36,29 @@ extern void cd_command(char* cur, char* path){
 		if (c == 0){
 			list_contents(cur);
 		}
-		else{
-		printf("%s> No Path Exists\n"	, cur);
+		else {
+			printf("%s> No Path Exists\n"	, cur);
 		}
 	}
 }
 
 extern void clear(void){
 	printf("\e[1;1H\e[2J");
-	}
+}
+
+extern void environ(void){
+	
+}
+
+extern void display_help(void){
+	printf("\nList of available commands:\n\n");
+    	printf("help\t\t\t Lists the help menu.\n");
+    	printf("dir <directory>\t\t Lists the contents of the directory\n");
+    	printf("environ\t\t\t Lists all the environment strings..\n");
+    	printf("echo <comment>\t\t Displays comment on the command line followed by a newline.\n");
+	printf("cd <path>\t\t Changes the working directory to the specified path.\n");
+    	printf("clr\t\t\t Clears the screen.\n");
+    	printf("pause\t\t\t Pauses operation of the shell until the ENTER key is hit.\n");
+    	printf("quit\t\t\t Quits the shell.\n\n\n");
+}
+
