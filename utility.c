a87d7d4c188233ac89ecf9883596e285e08b70ae
TaimourArshad1 <80366006+TaimourArshad1@users.noreply.github.com>
2022-03-10 21:40:25 -0500

    Update utility.c

--- utility.c
+++ utility.c
@@ -72,12 +72,20 @@ extern void environ(char shell[][BUFFER]){
 
 extern void display_help(void){
 	printf("\nList of available commands:\n\n");
-    	printf("help\t\t\t Lists the help menu.\n");
-    	printf("dir <directory>\t\t Lists the contents of the directory\n");
-    	printf("environ\t\t\t Lists all the environment strings..\n");
-    	printf("echo <comment>\t\t Displays comment on the command line followed by a newline.\n");
-	printf("cd <path>\t\t Changes the working directory to the specified path.\n");
-    	printf("clr\t\t\t Clears the screen.\n");
-    	printf("pause\t\t\t Pauses operation of the shell until the ENTER key is hit.\n");
-    	printf("quit\t\t\t Quits the shell.\n\n\n");
+    	printf("help: Lists the help menu.\n");
+    	printf("dir <directory>: Lists the contents of the directory\n");
+    	printf("environ: Lists all the environment strings..\n");
+    	printf("echo <comment>: Displays comment on the command line followed by a newline.\n");
+	printf("cd <path>: Changes the working directory to the specified path.\n");
+    	printf("clr: Clears screen.\n");
+    	printf("pause: Pauses operation of the shell until ENTER key is hit.\n");
+    	printf("quitQuits the shell.\n\n\n");
+}
+
+extern void pause_shell(void){
+    printf("\n\nShell has come to a halt. To continue, hit the ENTER key.....\n\n");
+    char code = (char)getchar();
+    while (code != '\n'){
+        code = (char)getchar();
+    }
 }
